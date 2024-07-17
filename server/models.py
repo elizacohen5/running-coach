69f791ab0d9377ea54aeb1c12e563be604c4842b
Eliza Cohen <elizacohen5@gmail.com>
2024-07-17 10:42:16 -0400

    added created_at and run_date rows to the future_runs table

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+from sqlalchemy import func
 
 db = SQLAlchemy()
 
@@ -26,10 +28,12 @@ class FutureRuns(db.Model, SerializerMixin):
       distance = db.Column(db.Integer)
       pace = db.Column(db.String)
       is_complete = db.Column(db.Boolean)
+      created_at = db.Column(db.DateTime, default=datetime.now())
+      run_date = db.Column(db.Date)
 
       runner = db.relationship('Runner', back_populates='runs')
 
       serialize_rules=['-runner.runs']
 
 def __repr__(self):
-        return f'<Runner {self.runner_id} {self.distance} {self.pace}>'
+        return f'<Runner: {self.runner_id} Distance: {self.distance} Pace: {self.pace} Created At: {self.created_at} Run Date: {self.run_date}>'
--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,6 @@
 # #!/usr/bin/env python3
 
+from datetime import date
 from app import app
 from models import db, Runner, FutureRuns
 
@@ -18,16 +19,16 @@ def run():
     db.session.commit()
 
     runs = [
-        FutureRuns(runner_id=runners[0].id, distance=3, pace='easy', is_complete=True),
-        FutureRuns(runner_id=runners[0].id, distance=7, pace='race', is_complete=True),
-        FutureRuns(runner_id=runners[0].id, distance=13, pace='long run', is_complete=False),
-        FutureRuns(runner_id=runners[1].id, distance=4, pace='threshold', is_complete=True),
-        FutureRuns(runner_id=runners[1].id, distance=6, pace='tempo', is_complete=False),
-        FutureRuns(runner_id=runners[1].id, distance=8, pace='easy', is_complete=False),
-        FutureRuns(runner_id=runners[2].id, distance=3, pace='threshold', is_complete=True),
-        FutureRuns(runner_id=runners[2].id, distance=1, pace='threshold', is_complete=True),
-        FutureRuns(runner_id=runners[2].id, distance=2, pace='easy', is_complete=True),
-        FutureRuns(runner_id=runners[0].id, distance=1, pace='tempo', is_complete=False)
+        FutureRuns(runner_id=runners[0].id, distance=3, pace='easy', is_complete=True, run_date=date(2024, 7, 17)),
+        FutureRuns(runner_id=runners[0].id, distance=7, pace='race', is_complete=True, run_date=date(2024, 7, 18)),
+        FutureRuns(runner_id=runners[0].id, distance=13, pace='long run', is_complete=False, run_date=date(2024, 7, 19)),
+        FutureRuns(runner_id=runners[1].id, distance=4, pace='threshold', is_complete=True, run_date=date(2024, 7, 18)),
+        FutureRuns(runner_id=runners[1].id, distance=6, pace='tempo', is_complete=False, run_date=date(2024, 7, 19)),
+        FutureRuns(runner_id=runners[1].id, distance=8, pace='easy', is_complete=False, run_date=date(2024, 7, 21)),
+        FutureRuns(runner_id=runners[2].id, distance=3, pace='threshold', is_complete=True, run_date=date(2024, 7, 22)),
+        FutureRuns(runner_id=runners[2].id, distance=1, pace='threshold', is_complete=True, run_date=date(2024, 7, 23)),
+        FutureRuns(runner_id=runners[2].id, distance=2, pace='easy', is_complete=True, run_date=date(2024, 7, 24)),
+        FutureRuns(runner_id=runners[0].id, distance=1, pace='tempo', is_complete=False, run_date=date(2024, 7, 30))
     ]
 
     db.session.add_all(runs)
