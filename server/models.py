742e33dc396ebc69f7b7f581c5a3b041b2a1b10a
Eliza Cohen <elizacohen5@gmail.com>
2024-07-21 13:57:34 -0400

    seed new tables

--- server/models.py
+++ server/models.py
@@ -14,11 +14,65 @@ class Runner(db.Model, SerializerMixin):
     password = db.Column(db.String(150), nullable=False)
 
     runs = db.relationship('FutureRuns', back_populates='runner')
+    current_conditioning = db.relationship('CurrentConditioning', back_populates='runner')
+    personal_records = db.relationship('PersonalRecords', back_populates='runner')
+    runner_goals = db.relationship('RunnerGoals', back_populates='runner')
 
-    serialize_rules=['-runs.runner']
+    serialize_rules=['-runs.runner', '-current_conditioning.runner', '-personal_records.runner', 'runner_goals.runner']
 
 def __repr__(self):
         return f'<Runner {self.name}>'
+
+class CurrentConditioning(db.Model, SerializerMixin):
+       __tablename__ = 'current_conditioning'
+
+       id = db.Column(db.Integer, primary_key=True)
+       runner_id = db.Column(db.Integer, db.ForeignKey('runners.id'))
+       runner_age = db.Column(db.Integer)
+       years_running = db.Column(db.Integer)
+       current_weekly_miles = db.Column(db.Integer)
+
+       runner = db.relationship('Runner', back_populates='current_conditioning')
+       serialize_rules=['-runner.current_conditioning']
+
+       def __repr__(self):
+        return f'<Runner Age: {self.runner_age} Years Running: {self.years_running} Weekly Mileage: {self.current_weekly_miles} >'
+
+class PersonalRecords(db.Model, SerializerMixin):
+       __tablename__ = 'personal_records'
+
+       id = db.Column(db.Integer, primary_key=True)
+       runner_id = db.Column(db.Integer, db.ForeignKey('runners.id'))
+       five_k_record = db.Column(db.Integer)
+       ten_k_record = db.Column(db.Integer)
+       half_marathon_record = db.Column(db.Integer)
+       marathon_record = db.Column(db.Integer)
+
+       runner = db.relationship('Runner', back_populates='personal_records')
+       serialize_rules=['-runner.personal_records']
+
+       def __repr__(self):
+        return f'<5k: {self.five_k_record} 10k: {self.ten_k_record} 1/2 marathon: {self.half_marathon_record} marathon: {self.marathon_record}>'
+
+class RunnerGoals(db.Model, SerializerMixin):
+     __tablename__ = 'runner_goals'
+
+     id = db.Column(db.Integer, primary_key=True)
+     runner_id = db.Column(db.Integer, db.ForeignKey('runners.id'))
+     race_training = db.Column(db.Boolean)
+     base_building = db.Column(db.Boolean)
+     weekly_mileage = db.Column(db.Integer)
+     weekly_sessions = db.Column(db.Integer)
+     weight_training = db.Column(db.Boolean)
+     cross_training = db.Column(db.Boolean)
+     race_date = db.Column(db.Date)
+
+     runner = db.relationship('Runner', back_populates='runner_goals')
+     serialize_rules=['-runner.runner_goals']
+
+     def __repr__(self):
+        return f'<Goals are Mileage: {self.weekly_mileage} Sessions: {self.weekly_sessions} Race Date: {self.race_date} >'
+
 
 class FutureRuns(db.Model, SerializerMixin):
       __tablename__ = 'future_runs'
